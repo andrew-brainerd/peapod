8f3ecd79d79d6f6e4194072d23246505f6fd087a
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2019-09-02 11:50:43 -0400

    Remove boilerplate

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Home.module.scss';
+
+const Home = () => (
+  <div className={styles.home}>
+    <div>Welcome to Peapod</div>
+    <div className={styles.happy}>:D</div>
+  </div>
+);
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import WebFont from 'webfontloader';
+import Home from './components/Home/Home';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+WebFont.load({
+  google: {
+    families: [
+      'Chilanka: 400',
+      'Roboto Mono: 400'
+    ]
+  }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Home />, document.getElementById('root'));
