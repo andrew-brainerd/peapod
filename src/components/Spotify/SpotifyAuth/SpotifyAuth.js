c615a332f97f7612665dcceacd3ae6464559006e
Andrew Brainerd <drwb333@gmail.com>
2020-01-25 11:31:52 -0500

    Add Spotify token expiration check

--- src/components/Spotify/SpotifyAuth/SpotifyAuth.js
+++ src/components/Spotify/SpotifyAuth/SpotifyAuth.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import { shape, string, func } from 'prop-types';
+import { setSpotifyAuth, getReturnUri } from '../../../utils/spotify';
 import { SPOTIFY_ROUTE } from '../../../constants/routes';
 import Loading from '../../common/Loading/Loading';
 import styles from './SpotifyAuth.module.scss';
 
 const SpotifyAuth = ({ query, navTo }) => {
   useEffect(() => {
-    // console.log(`Expires In: `, query.expiresIn);
-    const returnUri = localStorage.getItem('spotifyReturnUri');
+    setSpotifyAuth({
+      accessToken: query.access_token,
+      refreshToken: query.refresh_token,
+      expiresIn: query.expires_in
+    });
 
-    localStorage.setItem('spotifyAccessToken', query.access_token);
-    localStorage.setItem('spotifyRefreshToken', query.refresh_token);
-
-    navTo(returnUri || SPOTIFY_ROUTE);
+    navTo(getReturnUri() || SPOTIFY_ROUTE);
   });
 
   return (
@@ -26,7 +27,8 @@ const SpotifyAuth = ({ query, navTo }) => {
 SpotifyAuth.propTypes = {
   query: shape({
     access_token: string,
-    refresh_token: string
+    refresh_token: string,
+    expires_in: string
   }),
   navTo: func.isRequired
 };
--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -1,1 +1,48 @@
-export const getAccessToken = () => localStorage.getItem('spotifyAccessToken');
+import moment from 'moment';
+
+const SPOTIFY_RETURN_URI = 'spotifyReturnUri';
+const SPOTIFY_ACCESS_TOKEN = 'spotifyAccessToken';
+const SPOTIFY_REFRESH_TOKEN = 'spotifyRefreshToken';
+const SPOITFY_EXPIRE_TIME = 'spotifyExpireTime';
+
+export const getReturnUri = () => localStorage.getItem(SPOTIFY_RETURN_URI);
+
+const calculateExpireTime = expiresIn => {
+  console.log(`Calculate: `, expiresIn);
+  return moment().add(expiresIn, 'seconds').toString();
+};
+
+export const setSpotifyAuth = ({ accessToken, refreshToken, expiresIn }) => {
+  const expireTime = calculateExpireTime(expiresIn);
+
+  console.log(`%cSpotify Auth: %o`, 'color: green', { accessToken, refreshToken, expireTime });
+
+  localStorage.setItem(SPOTIFY_ACCESS_TOKEN, accessToken);
+  localStorage.setItem(SPOTIFY_REFRESH_TOKEN, refreshToken);
+  localStorage.setItem(SPOITFY_EXPIRE_TIME, expireTime);
+};
+
+const getExpireTime = currentMoment => {
+  const expireTime = localStorage.getItem(SPOITFY_EXPIRE_TIME);
+  expireTime ? console.log(`%cExpire Time: ${expireTime}`, 'color: green') : console.log(`%cNow: ${currentMoment.toString()}`, 'color: yellow');
+  return expireTime ? moment(expireTime) : currentMoment;
+};
+
+const getIsAuthExpired = currentMoment => {
+  const expireTime = getExpireTime(currentMoment);
+  const expireDiff = expireTime.diff(currentMoment);
+  console.log({ expireTime, expireDiff: moment.duration(expireDiff).asMinutes(), isExpired: moment.duration(expireDiff).asMinutes() < 5 });
+  return moment.duration(expireDiff).asMinutes() < 5;
+};
+
+export const getAccessToken = () => {
+  const isAuthExpired = getIsAuthExpired(moment());
+  const localAccessToken = localStorage.getItem(SPOTIFY_ACCESS_TOKEN);
+
+  isAuthExpired ? console.log('%cExpired', 'color: red') : console.log('%cGood', 'color: blue');
+
+  if (isAuthExpired) {
+    return null;
+  }
+  return localAccessToken;
+};
