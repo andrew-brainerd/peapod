927ab116d8d01b2c1dc0281e41dd6691a6fa4221
Andrew Brainerd <drwb333@gmail.com>
2020-01-25 00:17:16 -0500

    Add Spotify login button

--- src/components/Spotify/Spotify.js
+++ src/components/Spotify/Spotify.js
@@ -2,17 +2,28 @@ import React, { useEffect } from 'react';
 import { bool, array, string, oneOf, func } from 'prop-types';
 import { getAuth } from '../../api/spotify';
 import { podViews, SEARCH } from '../../constants/pods';
+import Button from '../common/Button/Button';
 import TrackList from './TrackList/container';
 import Player from './Player/container';
+import { ReactComponent as SpotifyIcon } from '../../img/spotify.svg';
+import styles from './Spotify.module.scss';
 
 const Spotify = ({ hasAuth, pathname, selectedView }) => {
   useEffect(() => {
-    !hasAuth && getAuth(pathname);
+    // !hasAuth && getAuth(pathname);
   }, [hasAuth, pathname]);
 
-  return selectedView === SEARCH ?
-    <TrackList /> :
-    <Player />;
+  return !hasAuth ?
+    <Button
+      className={styles.authButton}
+      onClick={() => getAuth(pathname)}
+    >
+      <SpotifyIcon />
+      <div className={styles.authButtonText}>Spotify Login</div>
+    </Button> :
+    selectedView === SEARCH ?
+      <TrackList /> :
+      <Player />;
 };
 
 Spotify.propTypes = {
