7df66392e74a5db7bc53295caa3e0015d03e3ce5
Andrew Brainerd <drwb333@gmail.com>
2020-01-28 12:59:47 -0500

    Add queueng modal

--- src/components/Spotify/Track/Track.js
+++ src/components/Spotify/Track/Track.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
+import noop from '../../../utils/noop';
 import styles from './Track.module.scss';
 
-const Track = ({ className, name }) => {
+const Track = ({ className, name, onClick }) => {
   return (
     <div className={[
       styles.track,
       className || ''
-    ].join(' ')}>
+    ].join(' ')}
+    onClick={onClick || noop}
+    >
       {name}
     </div>
   );
@@ -15,7 +18,8 @@ const Track = ({ className, name }) => {
 
 Track.propTypes = {
   className: string,
-  name: string
+  name: string,
+  onClick: func
 };
 
 export default Track;
--- src/components/Spotify/TrackList/TrackList.js
+++ src/components/Spotify/TrackList/TrackList.js
@@ -1,24 +1,58 @@
-import React, { useEffect } from 'react';
-import { bool, array, func } from 'prop-types';
+import React, { useState } from 'react';
+import { bool, array } from 'prop-types';
 import { isEmpty, uniqBy } from 'ramda';
+import Modal from '../../common/Modal/Modal';
+import Button from '../../common/Button/Button';
 import Track from '../Track/Track';
 import styles from './TrackList.module.scss';
 
 const TrackList = ({ hasAuth, isLoading, tracks }) => {
+  const [selectedTrack, setSelectedTrack] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return isLoading || !hasAuth ?
     <div className={styles.loading}>Loading Tracks...</div> :
-    <div className={styles.trackList}>
-      {!isEmpty(tracks) &&
-        <>
-          <div className={styles.list}>
-            <div className={styles.tracks}>
-              {uniqBy(track => track.name, tracks)
-                .map((track, t) => <Track key={t} {...track} />)}
+    <>
+      <div className={styles.trackList}>
+        {!isEmpty(tracks) &&
+          <>
+            <div className={styles.list}>
+              <div className={styles.tracks}>
+                {uniqBy(track => track.name, tracks)
+                  .map((track, t) =>
+                    <Track
+                      key={t}
+                      onClick={() => {
+                        setSelectedTrack(track);
+                        setIsModalOpen(true);
+                      }}
+                      {...track}
+                    />
+                  )}
+              </div>
             </div>
+          </>
+        }
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={() => setIsModalOpen(false)}
+        contentClassName={styles.trackModal}
+      >
+        <div className={styles.queueTrack}>
+          <div className={styles.songName}>
+            {(selectedTrack || {}).name}
           </div>
-        </>
-      }
-    </div>;
+          <Button
+            className={styles.queueButton}
+            text={'Queue Song'}
+            onClick={() => {
+              console.log('Queueing Song...');
+            }}
+          />
+        </div>
+      </Modal>
+    </>;
 };
 
 TrackList.propTypes = {
