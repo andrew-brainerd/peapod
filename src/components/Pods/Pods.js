44efc6c2fa64a1be1a933b11356b350b10c266eb
Andrew Brainerd <drwb333@gmail.com>
2020-02-15 20:59:12 -0500

    Add create button to pod list

--- src/components/Pods/Pods.js
+++ src/components/Pods/Pods.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { number, array, string, func } from 'prop-types';
-import PodItem from './PodItem/PodItem';
-import styles from './Pods.module.scss';
-import { POD_SEARCH_ROUTE } from '../../constants/routes';
 import { isEmpty } from 'ramda';
+import { POD_SEARCH_ROUTE, CREATE_POD_ROUTE } from '../../constants/routes';
 import Header from '../common/Header/container';
+import Button from '../common/Button/Button';
+import PodItem from './PodItem/PodItem';
+import { ReactComponent as NewIcon } from '../../img/add.svg';
+import styles from './Pods.module.scss';
 
 const getId = pod => (pod || {})._id;
 const getName = pod => pod && pod.name;
@@ -24,14 +26,22 @@ const Pods = ({ height, pods, userId, getMyPods, navTo }) => {
       <div className={styles.pods} style={{ height: podHeight }}>
         <div className={styles.podList} style={{ height: podHeight - 50 }}>
           {isEmpty(pods) ?
-            <div className={styles.loading}>Loading Pods...</div> :
-            (pods || []).map((pod, p) =>
-              <PodItem
-                key={p}
-                name={getName(pod)}
-                numMembers={getNumMembers(pod)}
-                action={() => navTo(POD_SEARCH_ROUTE.replace(':podId', getId(pod)))}
-              />
+            <div className={styles.loading}>Loading Pods...</div> : (
+              <>
+                <Button
+                  className={styles.createNew}
+                  onClick={() => navTo(CREATE_POD_ROUTE)}
+                >
+                  <NewIcon />
+                </Button>
+                {(pods || []).map((pod, p) =>
+                  <PodItem
+                    key={p}
+                    name={getName(pod)}
+                    numMembers={getNumMembers(pod)}
+                    action={() => navTo(POD_SEARCH_ROUTE.replace(':podId', getId(pod)))}
+                  />)}
+              </>
             )}
         </div>
       </div>
