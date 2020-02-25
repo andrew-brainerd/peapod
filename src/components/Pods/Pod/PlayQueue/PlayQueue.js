7671658b5c14f6210647a73761c51439c77bb21c
Andrew Brainerd <drwb333@gmail.com>
2020-02-24 23:01:41 -0500

    Add start playing queue button

--- src/components/Pods/Pod/PlayQueue/PlayQueue.js
+++ src/components/Pods/Pod/PlayQueue/PlayQueue.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { number, object, array } from 'prop-types';
+import { number, object, array, func } from 'prop-types';
 import { reverse } from 'ramda';
+import Button from '../../../common/Button/Button';
 import Track from '../../../Spotify/Track/Track';
 import styles from './PlayQueue.module.scss';
 
-const PlayQueue = ({ height, currentTrack, queue }) => {
+const PlayQueue = ({ height, currentTrack, queue, play }) => {
   const PLAYLIST_PADDING = 200;
 
   return (
     <div className={styles.playQueue} style={{ height: height - PLAYLIST_PADDING }}>
+      <Button
+        className={styles.startButton}
+        text={'Start Playing Queue'}
+        onClick={() => play(queue)}
+        disabled={!queue}
+      />
       <div className={styles.trackList}>
         {reverse(queue).map((track, t) => {
           return (currentTrack || {}).name !== track.name &&
@@ -22,7 +29,8 @@ const PlayQueue = ({ height, currentTrack, queue }) => {
 PlayQueue.propTypes = {
   height: number,
   currentTrack: object,
-  queue: array
+  queue: array,
+  play: func.isRequired
 };
 
 export default PlayQueue;
